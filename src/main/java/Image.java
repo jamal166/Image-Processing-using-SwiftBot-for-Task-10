d08763203f89ff5288ca0c350a5066948636e379
jamal166 <2423555@brunel.ac.uk>
2026-02-28 21:41:41 +0000

    Starting code for Underlights Testing

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -2,7 +2,12 @@ import swiftbot.*;
  
 public class Inc01_UnderlightsTest 
 {
-	
+	 private static volatile boolean xPressed = false;
+	    private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
+
+	    public static void main(String[] args) {
+	        new Inc01_UnderlightsTest().run();
+	    }
 }
 
 
