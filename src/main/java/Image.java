7923b7a1c8eeb9582b20b7a5dc310056dfda945b
jamal166 <2423555@brunel.ac.uk>
2026-02-28 21:43:46 +0000

    Still working on Underlights with inclusion of X for stop

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -8,6 +8,15 @@ public class Inc01_UnderlightsTest
 	    public static void main(String[] args) {
 	        new Inc01_UnderlightsTest().run();
 	    }
+	    
+	    private void run() {
+	        setupXButtonStop();
+	        System.out.println("[INC01] Underlights test. Press X to stop.");
+	        
+	        
+	        
+	    }
+
 }
 
 
