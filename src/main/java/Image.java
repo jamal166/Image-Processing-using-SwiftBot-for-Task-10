69f2084c66d4ab6600a3d7924057028797d21ed5
jamal166 <2423555@brunel.ac.uk>
2026-03-01 15:32:53 +0000

    Begining of increment 4

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -5,3 +5,8 @@ import java.awt.image.BufferedImage;
 import java.nio.file.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+public class Inc03_SaveOnePhoto {
+
+    private static volatile boolean xPressed = false;
+    private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
