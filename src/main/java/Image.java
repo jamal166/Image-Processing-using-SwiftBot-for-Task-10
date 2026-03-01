93c06af9377af4bef51e42cd11752819b6245821
jamal166 <2423555@brunel.ac.uk>
2026-03-01 16:13:09 +0000

    Image file saved location
    With function all our images are saved

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -10,3 +10,5 @@ public class Inc03_SaveOnePhoto {
 
     private static volatile boolean xPressed = false;
     private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
+    private static final Path IMAGE_DIR = Paths.get("/data/home/pi/task10/images");
+    private static final ImageSize SIZE = ImageSize.SQUARE_480x480;
