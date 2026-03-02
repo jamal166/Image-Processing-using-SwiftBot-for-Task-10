40053a57c7da5e7d8a44bae1342c3c998ab84da6
jamal166 <2423555@brunel.ac.uk>
2026-03-02 13:43:24 +0000

    Working on UltrasoundTest
    With this we are able to measure the distance of the object ahead of our SwifBot

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -1,42 +1,12 @@
 import swiftbot.*;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.nio.file.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class Inc03_SaveOnePhoto {
+public class Inc04_UltrasoundTest {
 
     private static volatile boolean xPressed = false;
     private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
-    private static final Path IMAGE_DIR = Paths.get("/data/home/pi/task10/images");
-    private static final ImageSize SIZE = ImageSize.SQUARE_480x480;
-    
+
+    private static final int DETECT_WITHIN_CM = 200;
+
     public static void main(String[] args) {
-        try {
-            new Inc03_SaveOnePhoto().run();
-        } catch (Exception e) {
-            System.out.println("[FATAL] " + e.getMessage());
-            e.printStackTrace();
-        }
+        new Inc04_UltrasoundTest().run();
     }
-    private void run() throws Exception {
-        setupXButtonStop();
-        Files.createDirectories(IMAGE_DIR);
-
-        System.out.println("[INC03] Taking one photo now... Press X to stop anytime.");
-        BufferedImage img = api.takeStill(SIZE);
-        String ts = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss").format(LocalDateTime.now());
-        Path out = IMAGE_DIR.resolve("inc03_" + ts + ".jpg");
-
-        ImageIO.write(img, "jpg", out.toFile());
-        System.out.println("[INC03] Saved: " + out);
-
-        api.disableAllButtons();
-    }
-    private void setupXButtonStop() {
-        api.disableAllButtons();
-        api.enableButton(Button.X, () -> xPressed = true);
-    }
-}
