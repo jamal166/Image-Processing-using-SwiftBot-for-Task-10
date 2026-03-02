8312094498962ea3136e67e7dbdf44bcb1ecf9d4
jamal166 <2423555@brunel.ac.uk>
2026-03-02 16:12:14 +0000

    Defining the Scaredy Mode
    to implement Scaredy mode behaviour only

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -6,3 +6,19 @@ import java.nio.file.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+public class Inc07_ScaredyOnly {
+
+    private static volatile boolean xPressed = false;
+    private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
+
+    private static final int SCAREDY_WITHIN_CM = 50;
+    private static final Path IMAGE_DIR = Paths.get("/data/home/pi/task10/images");
+    private static final ImageSize SIZE = ImageSize.SQUARE_480x480;
+
+    private static final int WANDER_L = 18;
+    private static final int WANDER_R = 20;
+
+    public static void main(String[] args) {
+        try { new Inc07_ScaredyOnly().run(); }
+        catch (Exception e) { e.printStackTrace(); }
+    }
