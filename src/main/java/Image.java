f2bf853601590cf979e9fb3e459acf1a2fc2671d
jamal166 <2423555@brunel.ac.uk>
2026-03-02 14:56:14 +0000

    Define class for  this increment
    of detecting and taking photos  and saving them

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -6,3 +6,15 @@ import java.nio.file.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+
+public class Inc06_EncounterSaveImage {
+
+    private static volatile boolean xPressed = false;
+    private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
+
+    private static final int DETECT_WITHIN_CM = 200;
+    private static final Path IMAGE_DIR = Paths.get("/data/home/pi/task10/images");
+    private static final ImageSize SIZE = ImageSize.SQUARE_480x480;
+
+    private static final int WANDER_L = 18;
+    private static final int WANDER_R = 20;
