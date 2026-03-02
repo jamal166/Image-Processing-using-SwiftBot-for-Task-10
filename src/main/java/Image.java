14b233a3b1796689875601b5b2c19bd3674fe4e5
jamal166 <2423555@brunel.ac.uk>
2026-03-02 14:04:45 +0000

    Define the class for  the Wondering

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -2,3 +2,18 @@ import swiftbot.*;
 
 import java.time.Duration;
 import java.time.Instant;
+public class Inc05_WanderNoObjectTurn {
+
+    private static volatile boolean xPressed = false;
+    private final SwiftBotAPI api = SwiftBotAPI.INSTANCE;
+
+    private static final int DETECT_WITHIN_CM = 200;
+
+    private static final Duration NO_OBJECT_TIMEOUT = Duration.ofSeconds(5);
+
+    private static final int WANDER_L = 18;
+    private static final int WANDER_R = 20;
+
+    public static void main(String[] args) {
+        new Inc05_WanderNoObjectTurn().run();
+    }
